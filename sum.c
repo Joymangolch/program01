14eb82c83e0c955e9686fe8efd3a708057c6d2d2
Joymangolch <147248110+Joymangolch@users.noreply.github.com>
2023-11-22 15:30:09 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -2,9 +2,9 @@
 int main()
 {
 int a,b,sum;
-printf("enter the two no. : ");
+printf("enter the two number : ");
 scanf("%d%d",&a,&b);
 sum=a+b;
-printf("the sum of two no. is : %d ",sum);
+printf("the sum of two number is : %d ",sum);
 return 0;
 }
